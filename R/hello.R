5337acc9c3f8128e5eeb13cd56d6b9906a7d09fd
Michael Hackl <michael.hackl@scinteco.com>
2022-10-19 13:50:50 +0200

    IMR-67

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,6 @@
 if (!is.null(get0(x = "BUILD",envir = globalenv()))) {
   ## do not forget to include in the DESCRIPTION
-  improveRcore:::buildMetaPackage("improveR",c("improveRcore","improveRget"))
+  buildMetaPackage("improveR",c("improveRcore","improveRget"))
 }
 
 
