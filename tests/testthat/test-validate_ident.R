35162fd631f7bc207a7d586438aac709ec2b8635
Roland Schmidt <roland.schmidt@scinteco.com>
2025-01-23 11:43:49 +0100

    Add unit tests for validate_ident function

---
+++ tests/testthat/test-validate_ident.R
@@ -0,0 +1,33 @@
+test_that("validate_ident returns NULL for empty character string", {
+  expect_null(validate_ident(""))
+})
+
+test_that("validate_ident returns NULL for empty list", {
+  expect_null(validate_ident(list()))
+})
+
+test_that("validate_ident returns NULL for empty data frame", {
+  expect_null(validate_ident(data.frame()))
+})
+
+test_that("validate_ident returns NULL for NULL input", {
+  expect_null(validate_ident(NULL))
+})
+
+test_that("validate_ident returns NULL for NA input", {
+  expect_null(validate_ident(NA))
+})
+
+test_that("validate_ident returns input for non-empty character string", {
+  expect_equal(validate_ident("non-empty"), "non-empty")
+})
+
+test_that("validate_ident returns input for non-empty list", {
+  expect_equal(validate_ident(list(1, 2, 3)), list(1, 2, 3))
+})
+
+test_that("validate_ident returns input for non-empty data frame", {
+  df <- data.frame(a = 1:3)
+  expect_equal(validate_ident(df), df)
+})
+
