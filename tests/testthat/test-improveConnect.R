8f1d26caf30ba95984819bcd45e65d20b416640b
Roland Schmidt <roland.schmidt@scinteco.com>
2025-03-17 11:03:05 +0100

    test of improveClose without cachEnv<-improveR::cachEnv works; earlier problem was due to missingelement specifications (path, target) for saveImproveJson(); 0E-0W-4N

--- tests/testthat/test-improveConnect.R
+++ tests/testthat/test-improveConnect.R
@@ -20,8 +20,6 @@
 
 test_that("improveConnected handles uninitialized state correctly", {
 
-  # cacheEnv <- improveR:::cacheEnv
-
   if (exists("cacheEnv")) {
     
     assign("initialized", FALSE, envir = cacheEnv)
@@ -76,7 +74,7 @@ test_that("improveDisconnect removes all connection information from the environ
 
 
 #improveClose
-test_that("improveClose handles file cleanup correctly", {
+test_that("OLD VERSION. improveClose handles file cleanup correctly", {
   
   improveR:::setRootPath(getwd())
 
@@ -92,7 +90,7 @@ test_that("improveClose handles file cleanup correctly", {
   writeLines(text="test2", con=pathFile2)
  
   # assign package cacheEnv to the environment of test function
-  # cacheEnv <- improveR:::cacheEnv
+  cacheEnv <- improveR:::cacheEnv
 
   cacheEnv$createdLinks <- data.frame(
     localPath = c(pathFile1, pathFile2, pathWithoutFile), 
@@ -107,6 +105,12 @@ test_that("improveClose handles file cleanup correctly", {
   expect_true("localPath" %in% names(cacheEnv$createdLinks))
   expect_true(file.exists(pathFile1))
   expect_true(file.exists(pathFile2))
+  
+  #adding this test shows that we are actually testing someting different
+  #we are testing a "copy" of the cacheEnv which we created when starting improveR;
+  #we are not testing bindings inside of improveR::cacheEnv; 
+  #but that's what the test should actually do
+  #expect_true(exists("createdLinks", envir=improveR::cacheEnv)) # TODO add test again later
 
   improveR::improveClose()
     
@@ -114,6 +118,59 @@ test_that("improveClose handles file cleanup correctly", {
   expect_true(is.environment(cacheEnv))
   expect_true(exists("createdLinks", envir=cacheEnv))
 
+
+  # files should be removed
+  expect_false(file.exists(pathFile1))
+  expect_false(file.exists(pathFile2))
+
+})
+
+# NOTE # @HACKLM
+# same test as above but with out cacheEnv assignment
+# as it turns out the initial problem was not about accessing the improveR::cacheEnv
+# in the tests, but correctly accessing/specifiyng the paths in the test since
+# saveImproveJson also requires path and target (not only pathLocal)
+# the tried approach cacheEnv<-improveR:::cacheEnv is not necessary and in fact is testing
+# something different than intended; it tests the presnece of specific bindings in a "copy" of improveR:::cacheEnv 
+# and not in improveR::cacheEnv itself. The approach below takes care of this
+test_that("ALTERNTAIVE - improveClose handles file cleanup correctly", {
+  
+  # improveR:::setRootPath(getwd()) #not needed
+
+  test_dir <- file.path(tempdir(), "test_improveClose")
+  dir.create(test_dir, showWarnings = FALSE)
+  on.exit(unlink(test_dir, recursive = TRUE)) #when exiting function remove folder and nested folders
+  
+  # Write test files to temp folder
+  pathFile1 <- file.path(test_dir, "file1.txt")
+  pathFile2 <- file.path(test_dir, "file2.txt")
+  pathWithoutFile <- file.path(test_dir, "Withoutfile.txt")
+  writeLines(text="test1", con=pathFile1) #store files with some content in the temp folder
+  writeLines(text="test2", con=pathFile2)
+ 
+  # assign package cacheEnv to the environment of test function
+  # cacheEnv <- improveR:::cacheEnv #REMOVED
+
+  cacheEnv$createdLinks <- data.frame(
+    localPath = c(pathFile1, pathFile2, pathWithoutFile), 
+    fileName=c(basename(pathFile1), basename(pathFile2), basename(pathWithoutFile)),
+    path = c(pathFile1, pathFile2, pathWithoutFile), 
+    target=c("res1","res2","res3"),
+    stringsAsFactors = FALSE
+  )
+
+  # Test before calling improveClose / files should exist
+  expect_true(exists("createdLinks", envir=cacheEnv))
+  expect_true("localPath" %in% names(cacheEnv$createdLinks))
+  expect_true(file.exists(pathFile1))
+  expect_true(file.exists(pathFile2))
+
+  improveR::improveClose()
+
+  # Tests after calling improveClose  
+  expect_true(exists("cacheEnv"))
+  expect_true(is.environment(cacheEnv))
+  expect_true(exists("createdLinks", envir=cacheEnv))
 
   # files should be removed
   expect_false(file.exists(pathFile1))
