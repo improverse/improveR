2f9e8371c2e96c6c446f60b3cd0e2bf2c7241f10
Roland Schmidt <roland.schmidt@scinteco.com>
2025-03-27 22:06:51 +0100

    start testing mocked httptest data

--- tests/testthat/test-loadResourceFromServer.R
+++ tests/testthat/test-loadResourceFromServer.R
@@ -1,16 +1,39 @@
-# TODO
+# TEST 
 httptest::with_mock_dir("loadResourceFromServerWorks", {
 
   test_that("loadResourceFromServerWorks", {
    
+    #Folder
+    
+    baseFileFolder <- loadResource(TEST_FOLDERS$baseFiles)
+    
+  
+    subFolders <- loadChildResources(baseFileFolder)$data[[1]]
+    subFolders$resourceId
+
+    Sys.setenv(IMPROVER_TEST_REPLAY="T")
+    Sys.setenv(IMPROVER_REPO_URL=mockUrl)
     improveConnect()
 
-    loadResourceFromServerResult <- loadResourceFromServer(resourceId = "robert_oracle-1:ST-63657")
     testthat::expect_s3_class(loadResourceFromServerResult, "data.frame")
     testthat::expect_true(nrow(loadResourceFromServerResult)==1)
 
   })
 })
+
+# # TEST WORKS WHEN RECORDING ON THE 4210 REPOSITORY
+# httptest::with_mock_dir("loadResourceFromServerWorks", {
+
+#   test_that("loadResourceFromServerWorks", {
+   
+#     improveConnect()
+
+#     loadResourceFromServerResult <- loadResourceFromServer(resourceId = "robert_oracle-1:ST-63657")
+#     testthat::expect_s3_class(loadResourceFromServerResult, "data.frame")
+#     testthat::expect_true(nrow(loadResourceFromServerResult)==1)
+
+#   })
+# })
 
 
 
