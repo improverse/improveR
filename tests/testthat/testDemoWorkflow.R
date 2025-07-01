d1a27e0844c9c8c8b84dc5c8c5c83991abcf199a
mhackl <michael.hackl@scinteco.com>
2025-07-01 15:45:14 +0100

    pipeline testing

--- tests/testthat/testDemoWorkflow.R
+++ tests/testthat/testDemoWorkflow.R
@@ -1,15 +1,15 @@
 
-resetCache()
-fullDemoWorkflow <- getFullLineage("envhost1.hc.scintecodev.internal-5310:ST-69749") %>%
-  makeStepsRelative() %>%
-  detachWorkflowFromResources() %>%
-  detachWorkflowFromTrees() %>%
-  setWorkflowTreeRootFolder("/demos/MWintegration3") %>%
-  retrieveWorkflow()
+#resetCache()
+#fullDemoWorkflow <- getFullLineage("envhost1.hc.scintecodev.internal-5310:ST-69749") %>%
+#  makeStepsRelative() %>%
+#  detachWorkflowFromResources() %>%
+#  detachWorkflowFromTrees() %>%
+#  setWorkflowTreeRootFolder("/demos/MWintegration3") %>%
+#  retrieveWorkflow()
 
 
-fullDemoWorkflow$breakpoint<-T
+#fullDemoWorkflow$breakpoint<-T
 
-jsonlite::write_json(fullDemoWorkflow,"demoWorkflow.json",pretty=T)
+#jsonlite::write_json(fullDemoWorkflow,"demoWorkflow.json",pretty=T)
 
-executeWorkflow(fullDemoWorkflow)
+#executeWorkflow(fullDemoWorkflow)
